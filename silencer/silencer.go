851e6fcdd93b6c28976cbce3dc8aef21adbb519c
twmb <travis.bischel@gmail.com>
2014-01-15 15:59:54 -0800

    silencer from @athomason
    Changed the way that the WrapSilenceFor func is called
    (the tag now is in the format filename:linenum passedid)
    
    I am open to changing it back.

---
+++ silencer/silencer.go
@@ -0,0 +1,83 @@
+package util
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"sync"
+	"time"
+)
+
+type SilencerState struct {
+	next     time.Time
+	count    int
+	lastFunc func(int, string)
+}
+
+type Silencer struct {
+	sync.Mutex
+	states map[string]*SilencerState
+}
+
+func NewSilencer() *Silencer {
+	return &Silencer{states: make(map[string]*SilencerState)}
+}
+
+var silencer = NewSilencer()
+
+// SilenceFor aggregates repeated calls to itself and calls f once every duration
+// with the number of aggregated calls and the id tagged with the calling file and
+// line number
+func SilenceFor(duration time.Duration, id string, f func(int, string)) {
+	// WrapSilenceFor is depth 1,
+	WrapSilenceFor(2, duration, id, f)
+}
+
+// WrapSilenceFor is the same as SilenceFor, except the depth of the call stack can be
+// chosen for what ID to tag and collate. runtime.Caller will have depth 0, and the
+// call to this function will have depth 1, so any additional layers before calling
+// this function should have depth >= 1.
+func WrapSilenceFor(depth int, duration time.Duration, id string, f func(int, string)) {
+	_, file, line, _ := runtime.Caller(depth)
+	file = filepath.Base(file)
+	key := fmt.Sprintf("%s:%d %s", file, line, id)
+
+	silencer.Lock()
+	state, exists := silencer.states[key]
+	if !exists {
+		state = new(SilencerState)
+	}
+
+	state.count++
+	state.lastFunc = f
+
+	if state.count == 1 {
+		now := time.Now()
+		if state.next.Before(now) {
+			// no need to suppress
+			state.lastFunc(state.count, key)
+			state.next = now.Add(duration)
+			state.count = 0
+		} else {
+			// this is the first time we've suppressed, so schedule the next
+			// firing. subsequent passes will only increment the counter.
+			go func() {
+				silencer.Lock()
+				wait := state.next.Sub(time.Now())
+				silencer.Unlock()
+
+				time.Sleep(wait)
+
+				silencer.Lock()
+				state.lastFunc(state.count, key)
+				state.next = time.Now().Add(duration)
+				state.count = 0
+				silencer.states[key] = state
+				silencer.Unlock()
+			}()
+		}
+	} // else state.count > 1 and a flush has already been scheduled
+
+	silencer.states[key] = state
+	silencer.Unlock()
+}
---
+++ silencer/silencer_test.go
@@ -0,0 +1,112 @@
+package util
+
+import (
+	"runtime"
+	"testing"
+	"time"
+)
+
+/*
+func TestSilencer1(t *testing.T) {
+	test(t, []string{""}, 1, 1000*time.Millisecond, 100*time.Millisecond, 11)
+}
+
+func TestSilencer2(t *testing.T) {
+	test(t, []string{"spot1", "spot2"}, 1, 1000*time.Millisecond, 100*time.Millisecond, 11)
+}
+
+func TestSilencer3(t *testing.T) {
+	test(t, []string{""}, 3, 1000*time.Millisecond, 100*time.Millisecond, 11)
+}
+
+func TestSilencer4(t *testing.T) {
+	test(t, []string{"#1", "#2"}, 3, 1000*time.Millisecond, 100*time.Millisecond, 11)
+}
+
+func TestSilencer5(t *testing.T) {
+	test(t, []string{"#1", "#2"}, 3, 10*time.Millisecond, 100*time.Millisecond, 2)
+}
+*/
+
+func test(t *testing.T, tags []string, invocations int, testTime time.Duration, silenceTime time.Duration, expectedPerInvocation int) {
+	var attempts, firings, errors int
+	lasts := make(map[string]time.Time)
+
+	f := func(count int, id string) {
+		last := lasts[id]
+		now := time.Now()
+		if last.IsZero() || now.Sub(last) >= silenceTime {
+			firings++
+		} else {
+			t.Logf("Error %q at %v; delta=%v attempts=%d last=%v count=%d",
+				id, time.Now(), now.Sub(last), attempts, last, count)
+			errors++
+		}
+		lasts[id] = now
+	}
+
+	expected := invocations * len(tags) * expectedPerInvocation
+
+	start := time.Now()
+	end := start.Add(testTime * 11 / 10)
+	for time.Now().Before(end) {
+		attempts++
+		if firings >= expected {
+			break
+		}
+		tag := tags[attempts%len(tags)]
+		// use separate calls so program counter is different for each
+		if invocations > 0 {
+			SilenceFor(silenceTime, tag, f)
+		}
+		if invocations > 1 {
+			SilenceFor(silenceTime, tag, f)
+		}
+		if invocations > 2 {
+			SilenceFor(silenceTime, tag, f)
+		}
+		runtime.Gosched() // yield to other goroutines
+	}
+
+	// wait for flusher goroutines to finish
+	finished := time.Now()
+	time.Sleep(2 * silenceTime)
+	finishedAndWaited := time.Now()
+
+	elapsed := finished.Sub(start)
+	longElapsed := finishedAndWaited.Sub(start)
+
+	t.Logf("Ran %d iterations in %v (%v with wait), fired correctly %d times (wanted %d) and %d incorrectly",
+		attempts, elapsed, longElapsed, firings, expected, errors)
+	if firings != expected {
+		t.Errorf("Expected %d firings, got %d", expected, firings)
+	}
+	if errors > 0 {
+		t.Errorf("Silencer failed to silence %d times", errors)
+	}
+}
+
+func TestSilencerStalled(t *testing.T) {
+	type Event struct {
+		time time.Time
+		n    int
+	}
+	events := make([]Event, 0)
+
+	// fire 5 events in rapid succession, all within the silence window. the
+	// first call should happen immediately but the next four should be
+	// coalesced at the end of the silence period.
+	start := time.Now()
+	for i := 0; i < 5; i++ {
+		SilenceFor(100*time.Millisecond, "anon", func(n int, tag string) {
+			events = append(events, Event{time.Now(), n})
+			t.Logf("%v", tag)
+		})
+		time.Sleep(10 * time.Millisecond)
+	}
+	time.Sleep(start.Add(110 * time.Millisecond).Sub(time.Now()))
+
+	if len(events) != 2 || events[0].n != 1 || events[1].n != 4 {
+		t.Errorf("unexpected event stream: %+v", events)
+	}
+}

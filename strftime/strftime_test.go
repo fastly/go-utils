4cf338765abcc443729d0fc8e86f149b550a104a
Jed Denlea <jed@fastly.com>
2014-07-01 17:28:38 -0700

    strftime: break build into linux/non-linux
    And change the tests to follow.
    
    In particular, strftime on OS X will yield slightly different results in
    the face of invalid format specifiers.
    
    Further, once you change a platform to use the Pure version, the
    existing Perl tests will only complain about the fact that you are not
    using the system version.

--- strftime/strftime_test.go
+++ strftime/strftime_test.go
@@ -5,8 +5,6 @@ import (
 
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 	"time"
 
 	"github.com/fastly/go-utils/strftime"
@@ -118,91 +116,6 @@ func testStrftimeAgainstReference(t *testing.T, impl strftimeImpl, strict bool) 
 			t.Logf("meh %d: `%s` => got `%s`, expected `%s`", i, test.format, got, test.expected)
 		} else {
 			t.Errorf("FAIL %d: `%s` => got `%s`, expected `%s`", i, test.format, got, test.expected)
-		}
-	}
-}
-
-// number of consecutive seconds to test, starting from refTime
-const (
-	duration int64 = 2 * 365 * 24 * 60 * 60
-	stride   int64 = 7 * 24 * 60 * 60
-)
-
-func TestStrftimeAgainstPerl(t *testing.T) {
-	testStrftimeAgainstPerl(t, strftime.Strftime, false)
-}
-
-// testStrftimePerl checks a range of times and uses the system's strftime(3)
-// as a reference for the correct answer. To access the system strftime, it
-// uses the core POSIX module of the first perl(1) found in the path. The test
-// can be skipped by setting the NO_PERL environment variable to non-empty.
-func testStrftimeAgainstPerl(t *testing.T, impl strftimeImpl, strict bool) {
-	if os.Getenv("NO_PERL") != "" {
-		t.Skip()
-		return
-	}
-
-	perlPath, err := exec.LookPath("perl")
-	if err != nil {
-		t.Logf("Couldn't locate perl, skipping: %s", err)
-		t.Skip()
-	}
-	perlOpts := []string{"-MPOSIX=strftime", "-E"}
-
-	origTimezone := os.Getenv("TZ")
-	var perlTests []test
-	for _, test := range tests {
-		if test.perlShouldWork {
-			perlTests = append(perlTests, test)
-		}
-	}
-	stmts := make([]string, len(perlTests))
-
-	for n := int64(0); n < duration; n += stride {
-		when := refTime + n
-		var tm = time.Unix(when, 0).In(tz)
-		/*
-			if n%100 == 0 {
-				log.Printf("Testing %d/%d (%.3f%%): %s", n+1, duration, 100*float64(n)/float64(duration), tm)
-			}
-		*/
-
-		// batch up all the tests for a single timestamp into one call out to perl
-		for i, test := range perlTests {
-			stmts[i] = fmt.Sprintf(`say strftime "%s", @t`, test.format)
-		}
-
-		prog := fmt.Sprintf("@t=localtime %d; %s", when, strings.Join(stmts, "; "))
-		args := append(perlOpts, prog)
-		//t.Logf("Executing perl: %s %v", perlPath, args)
-		os.Setenv("TZ", timezone)
-		output, err := exec.Command(perlPath, args...).CombinedOutput()
-		os.Setenv("TZ", origTimezone)
-		if err != nil {
-			t.Logf("perl failed: %s [%q]", err, output)
-			continue
-		}
-
-		results := strings.Split(string(output), "\n")
-		results = results[:len(results)-1] // strip trailing \n
-		if len(results) != len(perlTests) {
-			t.Fatalf("t=%d: got %d results from perl, expected %d: %q", when, len(results), len(perlTests), output)
-		}
-
-		for i, test := range perlTests {
-			got := strftime.Strftime(test.format, tm)
-			expected := results[i]
-			if got == string(expected) {
-				//t.Logf("ok %d/%d: `%s` => `%s`", i, when, test.format, got)
-			} else if test.localeDependent && !strict {
-				//t.Logf("meh %d/%d: `%s` => got `%s`, perl produced `%s`", i, when, test.format, got, expected)
-			} else {
-				t.Errorf("FAIL %d/%d: `%s` => got `%s`, perl produced `%s`", i, when, test.format, got, expected)
-			}
-		}
-
-		if testing.Short() {
-			break
 		}
 	}
 }
